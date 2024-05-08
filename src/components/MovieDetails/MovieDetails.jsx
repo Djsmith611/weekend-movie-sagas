8ab81321c83c13fb75c82cc5e6063e88b14c5029
David Smith <djsmith611@me.com>
2024-05-07 18:42:40 -0700

    addin genres to details

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
 
 export default function MovieDetails({movie}) {
+  const genres = useSelector(store => store.activeGenres);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch({ type:"GET_DETAILS", payload:movie.id });
+  }, [])
   return (
     <div>
       <h2>{movie.title}</h2>
       <img src={movie.poster} alt={movie.title} />
+      <ul>
+        {
+          genres.map((genre) => (
+            <li>
+              {genre.name}
+            </li>
+          ))
+        }
+      </ul>
       <p>{movie.description}</p>
     </div>
-      
   )
 }
