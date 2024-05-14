49ba7a056431cd2d48d176714acdf5d5cd24e235
David Smith <djsmith611@me.com>
2024-05-14 11:02:34 -0700

    reordered imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import './index.css';
-import store from './redux/store.js'
-import App from './components/App/App.jsx';
+import App from "./components/App/App.jsx";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import React from "react";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
