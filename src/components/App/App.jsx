edd124660360da556e680429c97d793f37229a2c
David Smith <djsmith611@me.com>
2024-05-14 10:34:08 -0700

    commented on App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Typography } from "@mui/material";
-import MovieList from "../MovieList/MovieList";
 import MovieDetails from "../MovieDetails/MovieDetails";
-import "./App.css";
+import MovieList from "../MovieList/MovieList";
+import { Typography } from "@mui/material";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./App.css";
 
+/**
+ * Main component/container for all application components
+ * @returns renders application
+ */
 function App() {
   return (
     <div className="App">
       <Router>
-        <Typography variant="h1" sx={{bgcolor:"black", color:"white"}}>The Movies Saga!</Typography>
+        <Typography variant="h1" sx={{ bgcolor: "black", color: "white" }}>
+          The Movies Saga!
+        </Typography>
         <Routes>
           <Route exact path="/" element={<MovieList />} />
           <Route path="/details/:id" element={<MovieDetails />} />
