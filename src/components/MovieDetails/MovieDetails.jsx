f5c874a88546ab42608377dc3daa62969d6b343e
David Smith <djsmith611@me.com>
2024-05-14 10:47:03 -0700

    added comments to MovieDetails

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,3 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
-import MovieDetailsEdit from "./MovieDetailsEdit";
 import {
   Box,
   Button,
@@ -12,28 +8,38 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
-import NavigationIcon from "@mui/icons-material/Navigation";
 import { usePalette } from "react-palette";
+import { useEffect, useState } from "react";
+import MovieDetailsEdit from "./MovieDetailsEdit";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import NavigationIcon from "@mui/icons-material/Navigation";
 
+
+// Displays the details page of a selected movie
 export default function MovieDetails() {
-  const [isEdit, setIsEdit] = useState(false);
-  const { id } = useParams();
-  const movie = useSelector((store) =>
+  const [isEdit, setIsEdit] = useState(false); // Edit mode state for details edit
+  const { id } = useParams(); // id parameter for displaying a single movies details
+  const movie = useSelector((store) => // selecting the movie based on parameters
     store.movies.find((item) => item.id === parseInt(id))
   );
-  const {
+  const { // react-palette palette generation based on the movie image ** Very fun!
     data: palette,
     loading: paletteLoading,
     error: paletteError,
   } = usePalette(`/${movie?.poster}`);
-  const genres = useSelector((store) => store.activeGenres);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const genres = useSelector((store) => store.activeGenres); // Selecting genres of the actively displayed movie
+  const dispatch = useDispatch(); // useDispatch hook
+  const navigate = useNavigate(); // useNavigate hook
 
-  useEffect(() => {
-    dispatch({ type: "GET_DETAILS", payload: parseInt(id) });
-  }, [dispatch, id]);
+  useEffect(() => { // Getting movie details on component load
+    const movieId = parseInt(id) // Ensuring 'id' is passsed as a number
+    dispatch({ type: "GET_DETAILS", payload: movieId });
+  }, [dispatch, id]); // using dispatch and id in the dependency array
 
+  /**
+   * using Circular progress as a loading screen in case movie is not loaded when component renders
+   */
   if (!movie) {
     return (
       <Box
@@ -49,13 +55,16 @@ export default function MovieDetails() {
     );
   }
 
+  // For navigation button to home
   const handleClick = () => {
     navigate("/");
   };
 
+  // For edit mode boolean
   const toggleEdit = () => {
     setIsEdit(!isEdit);
   }
+
 
   return (
     <div
@@ -67,6 +76,7 @@ export default function MovieDetails() {
       }}
     >
       <MovieDetailsEdit isEdit={isEdit} toggleEdit={toggleEdit} movie={movie} />
+      {/* Edit backdrop will be displayed if Edit mode is toggled */}
       <Typography variant="h2">{movie.title}</Typography>
       <img src={`/${movie.poster}`} alt={movie.title} style={{ height: "50vh" }} />
       <Grid
@@ -77,12 +87,13 @@ export default function MovieDetails() {
         marginY="5px"
         marginBottom="30px"
       >
-        {genres.map((genre) => (
+        {genres.map((genre) => ( // Mapping genres to Chip components under movie poster
           <Grid item xs="auto" key={genre.name}>
             <Chip label={genre.name} />
           </Grid>
         ))}
       </Grid>
+      {/* Placed edit button in divider for styling */}
       <Divider>Description<Button variant="text" onClick={toggleEdit}>Edit</Button></Divider>
       <Typography
         variant="body1"
@@ -102,6 +113,7 @@ export default function MovieDetails() {
       >
         {movie.description}
       </Typography>
+      {/* Using Fab and icon for navigation button to home */}
       <Fab variant="extended" data-testid="toList" onClick={handleClick}>
         <NavigationIcon sx={{ mr: 1 }} />
         Home
