744ca5445821401f17e578a02cee9addea58011e
David Smith <djsmith611@me.com>
2024-05-14 10:30:37 -0700

    commented on junction router

--- server/routes/junction.router.js
+++ server/routes/junction.router.js
@@ -2,12 +2,17 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
+/**
+ * GET genre names for actively displayed movie
+ * Utilizes junction table to reference the movies and genres tables.
+ * Sends genre names related to actively displayed movie
+ */
 router.get("/:id", (req, res) => {
   const sqlText = `
-        SELECT "genres"."name"
-        FROM "genres"
-        JOIN "movies_genres" ON "genres"."id" = "movies_genres"."genre_id"
-        WHERE "movies_genres"."movie_id" = $1;
+        SELECT "g"."name" -- selecting relevant genre names
+        FROM "genres" AS "g" -- using an alias
+        JOIN "movies_genres" AS "mg" ON "g"."id" = "mg"."genre_id" -- joining junction table
+        WHERE "mg"."movie_id" = $1; -- only where actively displayed movie is
     `;
   pool
     .query(sqlText, [req.params.id])
